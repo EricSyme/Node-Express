200b654656ca38fdd3173134c64a8f7dc36031b2
Eric Syme <spotlightphotoseattle@gmail.com>
2019-08-15 19:04:28 -0700

    Ext. Express Example

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use((req, res, next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
 });
 
 const server = http.createServer(app);
